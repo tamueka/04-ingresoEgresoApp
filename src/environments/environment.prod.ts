ba7761bccd307faef5ef7a7507c0689846e540bf
= <=>
2022-03-10 19:40:16 +0100

    disable button + enviromets firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDqVlYJHeQaWxOzaA2InPNNO154RaqGbyw',
+    authDomain: 'ingreso-egreso-app-720ff.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-720ff-default-rtdb.firebaseio.com',
+    projectId: 'ingreso-egreso-app-720ff',
+    storageBucket: 'ingreso-egreso-app-720ff.appspot.com',
+    messagingSenderId: '558307491378',
+    appId: '1:558307491378:web:2cd12af87676a76f1c862b',
+    measurementId: 'G-ZZSKWLX9L8',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDqVlYJHeQaWxOzaA2InPNNO154RaqGbyw',
+    authDomain: 'ingreso-egreso-app-720ff.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-720ff-default-rtdb.firebaseio.com',
+    projectId: 'ingreso-egreso-app-720ff',
+    storageBucket: 'ingreso-egreso-app-720ff.appspot.com',
+    messagingSenderId: '558307491378',
+    appId: '1:558307491378:web:2cd12af87676a76f1c862b',
+    measurementId: 'G-ZZSKWLX9L8',
+  },
 };
 
 /*
