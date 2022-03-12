0d8a9368f02f922a3d3bd11233092cd4d5db8691
= <=>
2022-03-12 14:54:25 +0100

    obtenemos informacion del usuario activo firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,14 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class AuthService {
   constructor(private auth: AngularFireAuth) {}
 
+  initAuthListener() {
+    this.auth.authState.subscribe((fuser) => {
+      console.log(fuser);
+      console.log(fuser?.uid);
+      console.log(fuser?.email);
+    });
+  }
+
   crearUsuario(nombre: string, correo: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(correo, password);
   }
