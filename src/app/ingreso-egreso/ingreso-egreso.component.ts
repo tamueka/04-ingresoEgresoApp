4ed52fc5b43e09448038085e5acab79b8d9c4b43
= <=>
2022-03-22 20:58:15 +0100

    pantalla ingreso-egresos, formulario reactivo + submit + botones

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class IngresoEgresoComponent implements OnInit {
+  ingresoForm: FormGroup;
+  tipo: string = 'Ingreso';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.ingresoForm = this.fb.group({
+      descripcion: ['', Validators.required],
+      monto: ['', Validators.required],
+    });
   }
 
+  guardar() {
+    if (this.ingresoForm.invalid) {
+      return;
+    }
+
+    console.log(this.ingresoForm);
+    console.log(this.ingresoForm.value);
+
+    console.log(this.tipo);
+  }
 }
