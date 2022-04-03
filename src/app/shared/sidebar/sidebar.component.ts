230b228530a00c4c45a1f7d3e53149dabe51fe29
= <=>
2022-04-03 18:03:55 +0200

    mostramos nombre usuario en sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,17 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { AppState } from 'src/app/app.reducer';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [],
 })
-export class SidebarComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+export class SidebarComponent implements OnInit, OnDestroy {
+  nombre = '';
+  userSubscription: Subscription;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userSubscription = this.store
+      .select('user')
+      .pipe(filter(({ user }) => user !== null))
+      .subscribe(({ user }) => {
+        this.nombre = user.nombre;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   logout() {
     this.authService.logout().then(() => {
